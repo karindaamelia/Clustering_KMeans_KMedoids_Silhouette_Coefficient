fc0c4bae48c1cab278f02d05d386afdd199fb2a2
Karinda Amelia <74167008+karindaamelia@users.noreply.github.com>
2024-07-25 06:40:42 +0700

    Create page K-Means and K-Medoids Clustering

---
+++ Src/pages/2_K-Means_Clustering.py
@@ -0,0 +1,30 @@
+import streamlit as st 
+
+from Utils.Session import get_session_state_instance
+from Utils.Clustering import Clustering
+
+def main():
+    # Get the singleton instance for session state
+    session_state = get_session_state_instance()
+
+    st.header("K-Means Clustering with Silhouette Coefficent")
+
+    # Get the processed dataset from the session state
+    preprocessing_dataset = session_state.preprocessing_dataset
+
+    # Check if the preprocessing dataset is available in 
+    if preprocessing_dataset is not None:
+        st.subheader("Preprocessing Dataset")
+        dataset = preprocessing_dataset
+        st.dataframe(dataset)
+        
+        # Integrated Silhouette
+        st.subheader("Silhouette Coefficient and Score")
+        clustering = Clustering(dataset)
+        clustering.apply_clustering(cluster_type='K-Means')  
+        
+    else:
+        st.warning("Preprocessing dataset is not available. Please go back to the homepage and preprocessing data.")
+
+if __name__ == "__main__":
+    main()
